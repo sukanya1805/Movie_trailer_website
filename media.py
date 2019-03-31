364a64d3a8990026bb3f716273e2a4298983b96f
Sukanya Srinivasan Iyer <46790907+sukanya1805@users.noreply.github.com>
2019-03-31 20:30:26 +0530

    Add files via upload

---
+++ media.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+
+class Movie:
+    '''Initializing values of instances'''
+    # VALID_RATINGS = ["G","PG","PG-13","R"] # Class variable
+
+    def __init__(movie_inst, title, storyline, posterimg, trailer, rating):
+        movie_inst.title = title
+        movie_inst.storyline = storyline
+        movie_inst.posterimg = posterimg
+        movie_inst.trailer = trailer
+        movie_inst.rating = rating
+
+    def show_trailer(movie_inst):
+        webbrowser.open(movie_inst.trailer) 
